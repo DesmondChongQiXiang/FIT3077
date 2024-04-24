8e770c8b71b9f082e68ebeae8cb728fae44e6e0c
Rohan <rsiv0010@student.monash.edu>
2024-04-24 18:05:26 +1000

    TEst push

---
+++ board/game_board.py
@@ -0,0 +1,39 @@
+import pygame
+from .settings import BLACK,ROWS,COLS,WHITE,SQUARE_SIZE
+
+class GameBoard:
+    def __init__(self,cave_tiles,chit_cards,players):
+        self.tiles = []
+        self.cave_tiles = cave_tiles
+        self.chit_cards = chit_cards
+        self.players = players
+        self.active_player = None
+        self.selected_chit_chard = None
+
+    def draw_board(self, window):
+        window.fill(BLACK)
+        
+        for row in range(ROWS):
+            if row == 1:
+                for col in range(1,COLS-1):
+                    pygame.draw.rect(window,WHITE,(row*SQUARE_SIZE,col*SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE))
+
+
+
+    def set_player_start_tile(self,players):
+        if len(self.players) == 4:
+            self.cave_tiles[0].place_player_on_tile(players[0])
+            self.cave_tiles[1].place_player_on_tile(players[1])
+            self.cave_tiles[2].place_player_on_tile(players[2])
+            self.cave_tiles[3].place_player_on_tile(players[3])
+        elif len(self.players) == 3:
+            self.cave_tiles[0].place_player_on_tile(players[0])
+            self.cave_tiles[1].place_player_on_tile(players[1])
+            self.cave_tiles[2].place_player_on_tile(players[2])
+        elif len(self.players) == 2:
+            self.cave_tiles[0].place_player_on_tile(players[0])
+            self.cave_tiles[2].place_player_on_tile(players[1])
+    
+    
+    def set_active_player(self,player):
+        self.active_player = player

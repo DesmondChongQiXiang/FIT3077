1982fc4217a7841982a5dbe199a655e930772cec
sjia0047 <sjia0047@student.monash.edu>
2024-04-25 01:46:58 +1000

    add capabiltiy to draw from list of asset drawing instructions

--- Sprint2_Game/screen/PygameScreenController.py
+++ Sprint2_Game/screen/PygameScreenController.py
@@ -1,13 +1,13 @@
-from __future__ import annotations
 from definitions import ROOT_PATH
 from typing import Optional
+from screen.DrawAssetInstruction import DrawAssetInstruction
 
 import pygame
 
 
 class PygameScreenController:
     """Contains useful methods for interacting with pygame's screen.
-    
+
     Warning: Should be used as a singleton
 
     Throws:
@@ -27,11 +27,11 @@ class PygameScreenController:
         if self.__screen is None:
             raise Exception("The pygame screen does not yet exist.")
 
-    def draw_image(self, image_path: str, x: int, y: int, width: Optional[int] = None, height: Optional[int] = None, rotate: float = 0) -> None:
-        """Draw an image on coordinates (x,y) on the screen.
+    def draw_asset(self, image_path: str, x: int, y: int, width: Optional[int] = None, height: Optional[int] = None, rotate: float = 0) -> None:
+        """Draw an asset on coordinates (x,y) on the screen.
 
         Args:
-            image_filename: The path relative to root directory
+            asset_path: The path to the asset relative to root directory
             x: x-coordinate
             y: y-coordinate
             width (optional): Width for the image
@@ -45,6 +45,27 @@ class PygameScreenController:
             image = pygame.transform.scale(image, (width, height))
         image = pygame.transform.rotozoom(image, rotate, 1.0)
         self.__screen.blit(image, (x, y))
+
+    def draw_assets_from_instructions(self, instructions: list[DrawAssetInstruction]) -> None:
+        """Draw assets based on instructions.
+
+        Args:
+            instructions: list containing drawing instructions
+
+        Author: Shen
+        """
+        for instruction in instructions:
+            self.draw_asset(instruction.get_asset_path(), instruction.get_x_coord(), instruction.get_y_coord(), instruction.get_width(), instruction.get_height(), instruction.get_rotation())
+
+    def get_screen_size(self) -> tuple[int, int]:
+        """Get the screen size.
+
+        Returns:
+            (width, height) of screen size
+
+        Author: Shen
+        """
+        return self.__screen.get_size()
 
 
 # References

6924e38c4f12cca4394017d45be60c03d7c90709
sjia0047 <sjia0047@student.monash.edu>
2024-04-26 15:52:37 +1000

    chit card size must now be specified on initialisation

--- Sprint2_Game/game_objects/chit_cards/AnimalChitCard.py
+++ Sprint2_Game/game_objects/chit_cards/AnimalChitCard.py
@@ -10,17 +10,16 @@ class AnimalChitCard(ChitCard):
     Author: Shen
     """
 
-    CHIT_CARD_PX_SIZE = 85  # pixel size for the chit card (L X W, where L = this value, W = this value)
-
-    def __init__(self, animal: Animal, symbol_count: int, coordinates: tuple[int, int]) -> None:
+    def __init__(self, animal: Animal, symbol_count: int, coordinates: tuple[int, int], size: tuple[int, int]) -> None:
         """
         Args:
             animal: The animal associated with the chit card
             symbol_count: The symbol count for the chit card
             coordinates: The coordinates to draw the chit cards at
+            size: (width, height) of the chit card in pixels
         """
         self.__animal = animal
-        super().__init__(symbol_count, coordinates)
+        super().__init__(symbol_count, coordinates, size)
 
     def get_draw_clickable_assets_instructions(self) -> list[tuple[DrawAssetInstruction, ModularClickableSprite]]:
         """Chit card displays back when it is not flipped. Otherwise display its front.
@@ -38,14 +37,14 @@ class AnimalChitCard(ChitCard):
                         f"{asset_path}/chit_card_{self.__animal.value}_{self.get_symbol_count()}.png",
                         x=coord_x,
                         y=coord_y,
-                        size=(AnimalChitCard.CHIT_CARD_PX_SIZE, AnimalChitCard.CHIT_CARD_PX_SIZE),
+                        size=self.get_size(),
                     ),
                     self,
                 )
             ]
         return [
             (
-                DrawAssetInstruction(f"{asset_path}/chit_card_back.png", x=coord_x, y=coord_y, size=(AnimalChitCard.CHIT_CARD_PX_SIZE, AnimalChitCard.CHIT_CARD_PX_SIZE)),
+                DrawAssetInstruction(f"{asset_path}/chit_card_back.png", x=coord_x, y=coord_y, size=self.get_size()),
                 self,
             )
         ]
--- Sprint2_Game/game_objects/chit_cards/ChitCard.py
+++ Sprint2_Game/game_objects/chit_cards/ChitCard.py
@@ -5,15 +5,17 @@ class ChitCard(ModularClickableSprite):
 
     Author: Shen
     """
-    def __init__(self, symbol_count: int, coordinates: tuple[int, int]) -> None:
+    def __init__(self, symbol_count: int, coordinates: tuple[int, int], size: tuple[int, int]) -> None:
         """
         Args:
             symbol_count: The symbol count for the chit card
             coordinates: The coordinates (x,y) to draw the chit cards at
+            size: (width, height) of the chit card in pixels
         """
         self.__symbol_count = symbol_count
         self.__flipped: bool = False
         self.__coordinates = coordinates
+        self.__size = size
 
     def set_flipped(self, state: bool) -> None:
         """Set the flipped state of the chit card."""
@@ -30,3 +32,11 @@ class ChitCard(ModularClickableSprite):
     def get_coordinates(self) -> tuple[int, int]:
         """Gets the coordinates in (x,y) at which the chit card should be drawn"""
         return self.__coordinates
+    
+    def get_size(self) -> tuple[int, int]:
+        """Gets the size of the chit card in pixels.
+        
+        Returns:
+            Size of the chit card in form (width, height) 
+        """
+        return self.__size

a52eb20b3d3f9660614818df632547d0136a21d7
sjia0047 <sjia0047@student.monash.edu>
2024-04-26 22:50:41 +1000

    CaveTile skeleton now takes in an animal

--- Sprint2_Game/game_objects/tiles/CaveTile.py
+++ Sprint2_Game/game_objects/tiles/CaveTile.py
@@ -1,12 +1,14 @@
 from game_objects.tiles.Tile import Tile
 from game_objects.characters.PlayableCharacter import PlayableCharacter
+from game_objects.tiles.TileDrawData import TileDrawData
+from game_objects.animals.Animal import Animal
 from screen.DrawAssetInstruction import DrawAssetInstruction
 from typing import Optional
 
 # TODO: Implement CaveTile
 
 class CaveTile(Tile):
-    def __init__(self, coordinates: tuple[int, int], size: tuple[int, int], character: Optional[PlayableCharacter] = None):
+    def __init__(self, animal: Animal, draw_data: Optional[TileDrawData] = None, character: Optional[PlayableCharacter] = None):
         pass
 
     def get_draw_assets_instructions(self) -> list[DrawAssetInstruction]:

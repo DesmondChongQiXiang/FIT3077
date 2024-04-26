48a6a5cab7d1fe87e4236b8dbbcb5d1f5082b275
sjia0047 <sjia0047@student.monash.edu>
2024-04-26 17:36:06 +1000

    tiles now contain the instructions for drawing themselves

--- Sprint2_Game/game_objects/tiles/NormalTile.py
+++ Sprint2_Game/game_objects/tiles/NormalTile.py
@@ -1,4 +1,32 @@
 from game_objects.tiles.Tile import Tile
+from game_objects.characters.PlayableCharacter import PlayableCharacter
+from game_objects.animals.Animal import Animal
+from screen.DrawAssetInstruction import DrawAssetInstruction
+from typing import Optional
+
 
 class NormalTile(Tile):
-    pass
+    """Represents a tile that has a representing animal.
+
+    Author: Shen
+    """
+
+    def __init__(self, coordinates: tuple[int, int], size: tuple[int, int], animal: Animal, character: Optional[PlayableCharacter] = None):
+        """
+        Args:
+            coordinates: The coordinate of the tile in form (x, y)
+            size: The size of the tile in pixels in form (width, height)
+            animal: The animal the tile represents
+            character (Optional): The character on the tile
+        """
+        self.__animal = animal
+        super().__init__(coordinates, size, character)
+
+    def get_draw_assets_instructions(self) -> list[DrawAssetInstruction]:
+        """Draw a game board tile.
+
+        Returns:
+            [Instruction to draw the game board tile]
+        """
+        x, y = self.get_coordinates()
+        return [DrawAssetInstruction("assets/game_board/game_board_tile.png", x, y, self.get_size())]
--- Sprint2_Game/game_objects/tiles/Tile.py
+++ Sprint2_Game/game_objects/tiles/Tile.py
@@ -1,4 +1,40 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Optional
+from screen.DrawableByAsset import DrawableByAsset
+from game_objects.characters.PlayableCharacter import PlayableCharacter
 
-class Tile(ABC):
-    pass
+# TODO: Incomplete
+
+
+class Tile(ABC, DrawableByAsset):
+    """Represents a tile that a character can stand on.
+
+    Author: Shen
+    """
+
+    def __init__(self, coordinates: tuple[int, int], size: tuple[int, int], character: Optional[PlayableCharacter] = None):
+        """
+        Args:
+            coordinates: The coordinate of the tile in form (x, y)
+            size: The size of the tile in pixels in form (width, height)
+            character (Optional): The character on the tile
+        """
+        self.__coordinates = coordinates
+        self.__size = size
+        self.__character = character
+
+    def get_coordinates(self) -> tuple[int, int]:
+        """Returns the coordinates of the tile.
+
+        Returns:
+            The coordinates of the tile in (x, y)
+        """
+        return self.__coordinates
+
+    def get_size(self) -> tuple[int, int]:
+        """Returns the size of the tile.
+
+        Returns:
+           The size of the tile in form (width px, height px)
+        """
+        return self.__size

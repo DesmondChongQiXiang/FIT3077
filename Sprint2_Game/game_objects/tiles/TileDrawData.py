5ee121f01641e7f3a5554fa19c0767576b2eba81
sjia0047 <sjia0047@student.monash.edu>
2024-04-26 22:13:11 +1000

    delete factories and create tile drawing data class

---
+++ Sprint2_Game/game_objects/tiles/TileDrawData.py
@@ -0,0 +1,30 @@
+class TileDrawData:
+    """A data class for organising the data required for drawing any tile.
+
+    Author: Shen
+    """
+
+    def __init__(self, coordinates: tuple[int, int], size: tuple[int, int]):
+        """
+        Args:
+            coordinates: The coordinates the tile should be draw at in form (x, y)
+            size: The size of the tile in pixels in form (width, height)
+        """
+        self.__coordinates = coordinates
+        self.__size = size
+
+    def get_coordinates(self) -> tuple[int, int]:
+        """Returns the coordinates the tile should be drawn at.
+
+        Returns:
+            The coordinates in form (x, y)
+        """
+        return self.__coordinates
+
+    def get_size(self) -> tuple[int, int]:
+        """Returns the dimensions that the tile should be drawn at.
+
+        Returns:
+            The size in form (width, height)
+        """
+        return self.__size
--- Sprint2_Game/game_objects/tiles/TileId.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class TileId(Enum):
-    """Represents identifiers for tile classes.
-    
-    Author: Shen
-    """
-    NORMAL = 1      # Identifier for NormalTile
-    CAVE = 2        # Identifier for CaveTile

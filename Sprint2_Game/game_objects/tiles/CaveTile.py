814de0713eb3121165cea2bf8e4bbe5b20c12944
sjia0047 <sjia0047@student.monash.edu>
2024-04-26 21:35:49 +1000

    cave tile skeleton to conform with Tile abstract class

--- Sprint2_Game/game_objects/tiles/CaveTile.py
+++ Sprint2_Game/game_objects/tiles/CaveTile.py
@@ -1,4 +1,13 @@
 from game_objects.tiles.Tile import Tile
+from game_objects.characters.PlayableCharacter import PlayableCharacter
+from screen.DrawAssetInstruction import DrawAssetInstruction
+from typing import Optional
+
+# TODO: Implement CaveTile
 
 class CaveTile(Tile):
-    pass
+    def __init__(self, coordinates: tuple[int, int], size: tuple[int, int], character: Optional[PlayableCharacter] = None):
+        pass
+
+    def get_draw_assets_instructions(self) -> list[DrawAssetInstruction]:
+        return []

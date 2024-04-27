e0442c421080ea113c1d8027e2eb03ba4cf9b52c
DesmondChongQiXiang <dcho0043@student.monash.edu>
2024-04-27 16:06:22 +1000

    crafted a class responsible for initializing the game and rendering the screen.

--- Sprint2/src/GameBoard.py
+++ Sprint2/src/GameBoard.py
@@ -1,78 +1,47 @@
 import pygame
 import random
-from constants import ROWS, COLS, CELL_WIDTH,CELL_HEIGHT, BLACK
+from constants import CELL_WIDTH,CELL_HEIGHT
+from ChitCard import ForwardChitCard, BackwardChitCard
+from Cave import Cave
+from Tile import Tile
+from Animal import Animal
+from DragonToken import DragonToken
 TILE_WIDTH = CELL_WIDTH - 5
 TILE_HEIGHT = CELL_HEIGHT - 5
 class GameBoard:
     def __init__(self):
-        self.board = []
+        self.occupiable = []
+        self.chit_card_list = []
+        self.cave = []
+        self.dragon_token = []
         self.turn = 0
 
-    def draw_square_tile(self,screen):
-        # top_row
-        top_row = ["babydragon","spider","babydragon","bat","spider","spider"]
-        self.traverse_cells(screen,1,1,1,6,top_row,1)
+    def create_tile(self, screen):
+        # top row
+        top_row = [Animal.BABYDRAGON,Animal.SPIDER,Animal.BABYDRAGON,Animal.BAT,Animal.SPIDER,Animal.SPIDER]
+        row_range = range(1,2)
+        col_range = range(1,7)
+        self.create_tiles_rowcolumn(screen, row_range, col_range, top_row)
 
-        right_column = ["bat","salamander", "salamander", "spider", "bat", "babydragon"]
-        self.traverse_cells(screen, 1, 7, 7, 7, right_column, 2,False)
+        # right column
+        right_column = [Animal.BAT,Animal.SALAMANDER,Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.BABYDRAGON]
+        row_range = range(1,7)
+        col_range = range(7,8)
+        self.create_tiles_rowcolumn(screen, row_range, col_range, right_column, False)
 
-        bottom_row = ["bat","babydragon","salamander","spider","bat","salamander"]
-        self.traverse_cells(screen, 7, 7, 1, 6, bottom_row, 2)
+        # bottom row
+        bottom_row = [None,Animal.BAT, Animal.BABYDRAGON,Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.SALAMANDER]
+        row_range = range(7,8)
+        col_range = range(7,1,-1)
+        self.create_tiles_rowcolumn(screen, row_range, col_range, bottom_row, True)
 
-        left_column = ["salamander", "babydragon", "spider","bat","salamander", "babydragon"]
-        self.traverse_cells(screen,1,7,1,1,left_column,2, False)
+        # left column
+        left_column = [None,Animal.SALAMANDER, Animal.BABYDRAGON,Animal.SPIDER,Animal.BAT,Animal.SALAMANDER,Animal.BABYDRAGON]
+        row_range = range(7,1,-1)
+        col_range = range(1,2)
+        self.create_tiles_rowcolumn(screen, row_range, col_range, left_column, False)
 
-    def draw_chit_card(self,screen,chit_card_list,index_list):
-        for i in range(len(chit_card_list)):
-            row,col = index_list[i]
-            image = pygame.image.load("../assets/{}".format(chit_card_list[i])).convert_alpha()
-            image = pygame.transform.scale(image, (TILE_WIDTH, TILE_HEIGHT))
-            screen.blit(image, (col * CELL_WIDTH + 2, row * CELL_HEIGHT + 2))
+    def draw_tiles(self,screen):
+        for i in range(len(self.occupiable)):
+            self.occupiable[i].draw(screen)
 
-    def create_chit_card(self):
-        animal_list = ["babydragon", "salamander", "spider", "bat"]
-        quantity_list = ["1", "2", "3"]
-        chit_card_list = []
-        for animal in animal_list:
-            for quantity in quantity_list:
-                chit_card_list.append("{}{}chitcard.png".format(quantity, animal))
-        for i in range(2):
-            chit_card_list.append("1piratedragonchitcard.png")
-            chit_card_list.append("2piratedragonchitcard.png")
-            random.shuffle(chit_card_list)
-        range_list = []
-        for i in range(2,7):
-            for j in range(2,7):
-                range_list.append((i,j))
-        random.shuffle(range_list)
-        index_list = []
-        for i in range(len(chit_card_list)):
-            index_list.append(range_list.pop())
-        return chit_card_list,index_list
-
-
-
-
-
-
-    def draw_animal(self,animal, x, y, size, color):
-        # Replace following with the actual drawing code for each animal
-        if animal == "BAT":
-            # Draw a bat on the tile
-            pass
-        elif animal == "SPIDER":
-            # Draw a spider on the tile
-            pass
-    def create_board(self):
-        pass
-
-    def traverse_cells(self,screen, row_start, row_end, col_start, col_end, img_name_list,i, select_col = True):
-        for row in range(row_start, row_end + 1):
-            for col in range(col_start, col_end + 1):
-                pygame.draw.rect(screen, BLACK, (col * CELL_WIDTH, row * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT), 2)
-                if select_col:
-                    image = pygame.image.load("../assets/{}tile.jpg".format(img_name_list[col - i])).convert_alpha()
-                else:
-                    image = pygame.image.load("../assets/{}tile.jpg".format(img_name_list[row - i])).convert_alpha()
-                image = pygame.transform.scale(image, (TILE_WIDTH, TILE_HEIGHT))
-                screen.blit(image, (col * CELL_WIDTH + 2, row * CELL_HEIGHT + 2))

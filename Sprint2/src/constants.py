38130ddd9b35a9f3fc306d8719279bc09963dc6a
DesmondChongQiXiang <dcho0043@student.monash.edu>
2024-04-27 16:04:56 +1000

    created a Tile class and implemented its functionality to display it on the screen. The constants file contains values that are utilized consistently throughout the game.

---
+++ Sprint2/src/constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH, HEIGHT = 1280, 720
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Number of rows and columns
+ROWS = 9
+COLS = 9
+
+# Calculate cell dimensions
+CELL_WIDTH = SCREEN_WIDTH // COLS
+CELL_HEIGHT = SCREEN_HEIGHT // ROWS
--- Sprint2/src/main.py
+++ Sprint2/src/main.py
@@ -1,32 +1,32 @@
-from Game import Game
 from constants import *
 from GameBoard import GameBoard
-# if __name__=="__main__":
-#     display = Display()
-#     game = Game(display)
-#     game.run()
 
 FPS = 60
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Fiery Dragon")
 gameboard = GameBoard()
 def main():
     running = True
     clock = pygame.time.Clock()
-    chit_card_list,index_list = gameboard.create_chit_card()
-    print(index_list)
+    gameboard.create_chit_card()
+    gameboard.create_tile(screen)
+    gameboard.create_caves(4)
+    gameboard.create_dragon_token(4)
     while running:
         clock.tick(FPS)
-        screen.fill(BLACK)
+        screen.fill((0,0,0))
+        menu_mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        tile_size = 100
-        gameboard.draw_square_tile(screen)
-        gameboard.draw_chit_card(screen,chit_card_list,index_list)
+                for chitcard in gameboard.chit_card_list:
+                    chitcard.checkforInput(menu_mouse_pos)
+        gameboard.draw_tiles(screen)
+        gameboard.draw_chit_card(screen)
+        gameboard.draw_caves(screen)
+        gameboard.draw_dragon_token(screen)
         pygame.display.update()
     pygame.quit()
 

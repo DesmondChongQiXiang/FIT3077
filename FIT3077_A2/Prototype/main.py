f1928cb73e70949df23d2bd19d49ad9c51b41975
Rohan <rsiv0010@student.monash.edu>
2024-04-28 18:48:49 +1000

    flipping now works when a chit card is clicked on. REQ 2 complete. Todo : Add the players to the caves and create the list representing the gameboard player positions

--- FIT3077_A2/Prototype/main.py
+++ FIT3077_A2/Prototype/main.py
@@ -1,19 +1,25 @@
 import pygame
-from board.settings import WIDTH,HEIGHT
+from board.settings import WIDTH,HEIGHT,SQUARE_SIZE
 from board.game_board import GameBoard
 pygame.init()
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Fiery Dragons')
 FPS = 60
 
+def get_row_col_from_mouse(pos):
+    x,y = pos
+    row = x // SQUARE_SIZE
+    col = y // SQUARE_SIZE
+    return (row,col)
+
 def main():
     run = True
     clock = pygame.time.Clock()
     board = GameBoard([],[],[])
     board.shuffle_chit_cards()
-    chit_card = board.get_piece(2,6)
-    print(chit_card.x,chit_card.y)
-    board.flip_chit_card(chit_card)
+    #chit_card = board.get_piece(2,6)
+    #print(chit_card.x,chit_card.y)
+    #board.flip_chit_card(chit_card)
 
     while run:
         clock.tick(FPS)
@@ -22,7 +28,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                position = pygame.mouse.get_pos()
+                row,col = get_row_col_from_mouse(position)
+                chit_card = board.get_piece(row,col)
+                board.flip_chit_card(chit_card)
         board.draw_game(WINDOW)
         pygame.display.update()
 

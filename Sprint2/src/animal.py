fd4ef344756a08d6ac80ebb8aefa31988970a236
DesmondChongQiXiang <dcho0043@student.monash.edu>
2024-04-29 12:20:37 +1000

    created animal enum class for representing animal

---
+++ Sprint2/src/animal.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Animal(Enum):
+    """
+    Animal Enum
+
+    Represents different types of animals on chit card and cave
+
+    Enum Values:
+        BABYDRAGON: A baby dragon.
+        SALAMANDER: A salamander.
+        SPIDER: A spider.
+        BAT: A bat.
+        DRAGONPIRATE: A dragon pirate.
+    """
+    BABYDRAGON = "BABYDRAGON" # Enum value representing a baby dragon
+    SALAMANDER = "SALAMANDER" # Enum value representing a salamander
+    SPIDER = "SPIDER" # Enum value representing a spider
+    BAT = "BAT" # Enum value representing a bat
+    DRAGONPIRATE = "DRAGONPIRATE" # Enum value representing a dragon pirate

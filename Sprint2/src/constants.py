416f582fcb49f65b03f56a3be65b9cb8c8cb18b8
DesmondChongQiXiang <dcho0043@student.monash.edu>
2024-04-29 13:18:23 +1000

    hardcode the position of caves, chitcard position range, dragontoken position, tiles position and animal order, caves position and animal order in constants.py instead of gameboard to make the system maintainable

--- Sprint2/src/constants.py
+++ Sprint2/src/constants.py
@@ -1,3 +1,4 @@
+from animal import Animal
 # Window Configuration
 WIDTH, HEIGHT = 1280, 720 # Width and height of the game window
 SCREEN_WIDTH = 1280 # Width of the game screen
@@ -10,3 +11,33 @@ COLS = 9 # Number of columns in the grid
 # Calculate cell dimensions
 CELL_WIDTH = SCREEN_WIDTH // COLS # Width of each grid cell
 CELL_HEIGHT = SCREEN_HEIGHT // ROWS # Height of each grid cell
+
+# the number of tiles
+TILES_NUM = 24
+# Define the tiles row and column order from top-left clockwise until the end
+TILES_ROW_COLUMN = [(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),
+                    (1,7),(2,7),(3,7),(4,7),(5,7),(6,7),
+                    (7,7),(7,6),(7,5),(7,4),(7,3),(7,2),
+                    (7,1),(6,1),(5,1),(4,1),(3,1),(2,1)]
+# tiles position
+TILES_POS = [(row_column[1] * CELL_WIDTH, row_column[0] * CELL_HEIGHT) for row_column in TILES_ROW_COLUMN]
+
+# Define the tiles animal order from top-left clockwise until the end
+TILES_ANIMAL_ORDERING = [Animal.BABYDRAGON,Animal.SPIDER,Animal.BABYDRAGON,Animal.BAT,Animal.SPIDER,Animal.SPIDER,
+                         Animal.BAT,Animal.SALAMANDER,Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.BABYDRAGON,
+                         Animal.SALAMANDER,Animal.BAT,Animal.SPIDER,Animal.SALAMANDER,Animal.BABYDRAGON,Animal.BAT,
+                         Animal.BABYDRAGON,Animal.SALAMANDER,Animal.BAT,Animal.SPIDER,Animal.BABYDRAGON,Animal.SALAMANDER]
+
+# caves and dragon row and column
+CAVES_DRAGON_ROW_COLUMN = [(4, 0), (4, 8), (0, 4), (8, 4)]
+# caves and dragonposition
+CAVES_DRAGON_POS = [(row_column[1] * CELL_WIDTH, row_column[0] * CELL_HEIGHT) for row_column in CAVES_DRAGON_ROW_COLUMN]
+# caves's animal(from cave 1 to cave 4)
+CAVES_ANIMAL = [Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.BABYDRAGON]
+
+# chit card row position range
+CHITCARD_ROW_RANGE = range(2,7)
+# chit card column position range
+CHITCARD_COL_RANGE = range(2,7)
+
+
--- Sprint2/src/gameboard.py
+++ Sprint2/src/gameboard.py
@@ -1,5 +1,5 @@
 import random
-from constants import CELL_WIDTH,CELL_HEIGHT
+from constants import CELL_WIDTH,CELL_HEIGHT, TILES_ANIMAL_ORDERING,TILES_POS,TILES_NUM,CAVES_ANIMAL,CAVES_DRAGON_POS,CHITCARD_COL_RANGE,CHITCARD_ROW_RANGE
 from chitcard import ForwardChitCard, BackwardChitCard
 from cave import Cave
 from tile import Tile
@@ -28,54 +28,12 @@ class GameBoard:
         Returns:
             list: List of tile objects.
         """
+
         tiles = [] # tiles list for return back to game class
-        # top row of the animal order
-        top_row = [Animal.BABYDRAGON,Animal.SPIDER,Animal.BABYDRAGON,Animal.BAT,Animal.SPIDER,Animal.SPIDER]
-        row_range = range(1,2)  # loop through row 1
-        col_range = range(1,7)  # loop through column 1 to column 7
-        tiles.extend(self._create_tiles_rowcolumn(row_range, col_range, top_row)) # extend the returned list into the tiles list
-
-        # right column of animal order
-        right_column = [Animal.BAT,Animal.SALAMANDER,Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.BABYDRAGON]
-        row_range = range(1,7) # loop through row 1 to 7
-        col_range = range(7,8) # only focus on column 7
-        tiles.extend(self._create_tiles_rowcolumn(row_range, col_range, right_column, False))
-
-        # bottom row of animal order
-        bottom_row = [None,Animal.BAT, Animal.BABYDRAGON,Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.SALAMANDER]
-        row_range = range(7,8) # only focus on row 7
-        col_range = range(7,1,-1) # loop through column 2 to 7 reversely
-        tiles.extend(self._create_tiles_rowcolumn(row_range, col_range, bottom_row))
-
-        # left column
-        left_column = [None,Animal.SALAMANDER, Animal.BABYDRAGON,Animal.SPIDER,Animal.BAT,Animal.SALAMANDER,Animal.BABYDRAGON]
-        row_range = range(7,1,-1) # loop through row 2 to 7 reversely
-        col_range = range(1,2) # only focus on column 1
-        tiles.extend(self._create_tiles_rowcolumn(row_range, col_range, left_column, False))
-        self.drawables.extend(tiles) # also extend to drawable list to draw the gameboard later
-        return tiles # return the obtained tiles back to game class
-
-    def _create_tiles_rowcolumn(self, row_range, col_range, animal_list, is_row = True):
-        """
-        Creates and returns a list of tile objects based on row and column ranges.
-
-        Args:
-            row_range (range): Range of rows.
-            col_range (range): Range of columns.
-            animal_list (list): List of animals for the tiles.
-            is_row (bool): Whether we are loop through row or column
-
-        Returns:
-            list: List of tile objects.
-        """
-        tiles = [] # list of tile for row or column
-        for row in row_range:
-            for col in col_range:
-                if is_row:
-                    tile = Tile(animal_list[col - 1], col * CELL_WIDTH, row * CELL_HEIGHT) # create tile with ordering of animal and the position
-                else:
-                    tile = Tile(animal_list[row - 1], col * CELL_WIDTH, row * CELL_HEIGHT)
-                tiles.append(tile)
+        for i in range(TILES_NUM):
+            tile = Tile(TILES_ANIMAL_ORDERING[i], TILES_POS[i][0], TILES_POS[i][1])
+            tiles.append(tile)
+        self.drawables.extend(tiles)
         return tiles
 
 
@@ -86,7 +44,7 @@ class GameBoard:
         Returns:
             list: List of randomized positions.
         """
-        range_list = [(i, j) for i in range(2, 7) for j in range(2, 7)] # the available position for chitcards is only from row 2 to 7 and column 2 to 7
+        range_list = [(i, j) for i in CHITCARD_ROW_RANGE for j in CHITCARD_COL_RANGE] # the available position for chitcards is only from row 2 to 7 and column 2 to 7
         rowcol_list = random.sample(range_list, 16) # get 16 random position because we have only 16 chitcards
 
         # Pre-calculate positions
@@ -132,11 +90,8 @@ class GameBoard:
             list: List of cave objects.
         """
         caves = [] # a list to store caves to return back to game class
-        caves_animal = [Animal.SALAMANDER,Animal.SPIDER,Animal.BAT,Animal.BABYDRAGON] # the ordering of animal from cave1 to cave4 have
-        cave_pos = [(4,0),(4,8),(0,4),(8,4)] # the row and column of the cave1 to cave 4
-        cave_pos = [(pos[1] * CELL_WIDTH, pos[0] * CELL_HEIGHT) for pos in cave_pos] # calculate the actual position on the screen
         for i in range(player_num): # create the number of caves based on the number of players
-            caves.append(Cave(i+1,caves_animal[i],cave_pos[i])) # create cave with id, animal and position
+            caves.append(Cave(i+1,CAVES_ANIMAL[i],CAVES_DRAGON_POS[i])) # create cave with id, animal and position
         self.drawables.extend(caves) # also extend to drawable list to draw the gameboard later
         return caves # return the obtained caves back to game class
 
@@ -151,10 +106,8 @@ class GameBoard:
             list: List of dragon token objects.
         """
         dragon_tokens = [] # a list to store dragon token to return back to game class
-        dragon_token_pos = [(4, 0), (4, 8), (0, 4), (8, 4)] # the row and column of the dragontoken1 to dragontoken4
-        dragon_token_pos = [(pos[1] * CELL_WIDTH, pos[0] * CELL_HEIGHT) for pos in dragon_token_pos] # calculate the actual position on the screen
         for i in range(player_num): # create the number of dragon token based on the number of players
-            dragon_tokens.append(DragonToken(i + 1, dragon_token_pos[i])) # create dragontoken with id and position
+            dragon_tokens.append(DragonToken(i + 1, CAVES_DRAGON_POS[i])) # create dragontoken with id and position
         self.drawables.extend(dragon_tokens) # also extend to drawable list to draw the gameboard later
         return dragon_tokens # return the obtained dragon token back to game class
 

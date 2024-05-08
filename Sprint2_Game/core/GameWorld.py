b03f8a35c4047a4b272d90ab2ac19fe10764bd2e
sjia0047 <sjia0047@student.monash.edu>
2024-05-09 02:00:42 +1000

    created player turn foundation

--- Sprint2_Game/core/GameWorld.py
+++ Sprint2_Game/core/GameWorld.py
@@ -15,13 +15,22 @@ class GameWorld:
     """
 
     def __init__(self, playable_characters: list[PlayableCharacter], game_board: GameBoard):
-        """
+        """Constructs the game world with the first character in the list of playable characters being the starting player.
+
         Args:
             playable_charcters: The list of playable characters to initialise the world (game) with
             game_board: The game board
+
+        Throws:
+            Exception when the number of playable characters is less than 2.
         """
+        if len(playable_characters) < 2:
+            raise ValueError("Number of playable characters must be at least 2.")
+
         self.__playable_characters: list[PlayableCharacter] = playable_characters
         self.__game_board: GameBoard = game_board
+        self.__current_player = playable_characters[0]
+        self.__current_player_i = 0  # for turn purposes only
 
         self.__run()
 
@@ -40,9 +49,6 @@ class GameWorld:
             PygameScreenController_instance().draw_assets_from_instructions(self.__game_board.get_draw_assets_instructions())
             chit_card_hitboxes = PygameScreenController_instance().draw_clickable_assets_from_instructions(self.__game_board.get_draw_clickable_assets_instructions())
 
-            # Handle Turns
-            current_player: PlayableCharacter = self.__playable_characters[0]  # TEMP CODE
-
             # Handle Events
             for event in pygame.event.get():
                 match event.type:
@@ -51,7 +57,10 @@ class GameWorld:
                         return
 
                     case pygame.MOUSEBUTTONDOWN:  # handle mouse click
-                        self.__fire_onclick_for_clicked_hitboxes(chit_card_hitboxes, current_player)
+                        self.__fire_onclick_for_clicked_hitboxes(chit_card_hitboxes, self.__current_player)
+
+            # Handle Player Turns
+            self.__process_current_player_turn()
 
             # Update screen & Set FPS
             pygame.display.flip()  # update screen
@@ -70,3 +79,16 @@ class GameWorld:
             pos = pygame.mouse.get_pos()
             if rect.collidepoint(pos):
                 clickable.on_click(player, players_tile)
+
+    def __process_current_player_turn(self) -> None:
+        """Ends the current player's turn and transitions turn to the next player if their turn should end. Otherwise does
+        nothing."""
+        if not self.__current_player.should_continue_turn():
+            if self.__current_player_i + 1 > len(self.__playable_characters) - 1:
+                # if the current player was the last player, loop around to the starting player
+                self.__current_player = self.__playable_characters[0]
+                self.__current_player_i = 0
+            else:
+                # otherwise turn should go to next player
+                self.__current_player = self.__playable_characters[self.__current_player_i + 1]
+                self.__current_player_i += 1

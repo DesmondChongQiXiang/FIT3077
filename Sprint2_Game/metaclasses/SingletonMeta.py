063594999a8bcbcfe7b065191b8510f3638619ef
sjia0047 <sjia0047@student.monash.edu>
2024-05-10 00:45:29 +1000

    remove redundant line and rename variable to clearly reflect its purpose in SingletonMeta

--- Sprint2_Game/metaclasses/SingletonMeta.py
+++ Sprint2_Game/metaclasses/SingletonMeta.py
@@ -1,20 +1,21 @@
 class SingletonMeta(type):
     """Metaclass that allows for only one instance of each class user. Instantiating another instance of the same class,
-    even with different parameters will always return the original singleton."""
+    even with different parameters will always return the original singleton.
 
-    _instances = {}  # static class variable keeping track of all the singletons of varying classes
+    Author: Shen
+    """
+
+    __singletons = {}  # static class variable keeping track of all the singletons of varying classes
 
     def __call__(cls, *args, **kwargs):
         """Instantiate the class defining their metaclass as this class.
 
         Args:
-            cls: The class using SingletonMeta
+            cls: The class using the SingletonMeta
             *args, **kwargs: Arguments and key-word arguments passed in to instantiate class
         """
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)  # create the class object
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+        if cls not in cls.__singletons:
+            cls.__singletons[cls] = super().__call__(*args, **kwargs)  # create the class object
+        return cls.__singletons[cls]
 
-
-# ADAPTED FROM: https://refactoring.guru/design-patterns/singleton/python/example#example-0
+# ADAPTED FROM: https://refactoring.guru/design-patterns/singleton/python/example 

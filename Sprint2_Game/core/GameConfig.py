8b9c8d66027d7c8c6497d39475223fce50848f74
sjia0047 <sjia0047@student.monash.edu>
2024-05-18 00:14:37 +1000

    fix error in for windows description for GameConfig REQUESTED_SCREEN_SIZE

--- Sprint2_Game/core/GameConfig.py
+++ Sprint2_Game/core/GameConfig.py
@@ -3,7 +3,7 @@
 # ===== SCREEN ==========================================================================================================================
 
 # REQUESTED_SCREEN_SIZE
-# For Windows - Screen size of the game will automatically adjust to your shortest dimension (height / width) if one of them is exceeded
+# For Windows - Screen size of the game will automatically adjust to your shortest dimension (height / width) if exceeded
 # For other OS - Specifies the screen size directly
 REQUESTED_SCREEN_SIZE = 1500
 

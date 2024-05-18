6265735c5321580916511807a9a2bce71e89a7ab
sjia0047 <sjia0047@student.monash.edu>
2024-05-18 22:46:37 +1000

    formatting: White spaces for Animal

--- Sprint2_Game/game_objects/animals/Animal.py
+++ Sprint2_Game/game_objects/animals/Animal.py
@@ -1,11 +1,13 @@
 from enum import Enum
 
+
 class Animal(Enum):
-    """Enum representing the animals that appear on tiles and chit cards. 
-        
+    """Enum representing the animals that appear on tiles and chit cards.
+
     Author: Shen
     """
-    SALAMANDER = "salamander"
+
     BABY_DRAGON = "baby_dragon"
+    SALAMANDER = "salamander"
     SPIDER = "spider"
     BAT = "bat"

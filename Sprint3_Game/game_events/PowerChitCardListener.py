b40120645c3738c42fa08a8a88bfbedf1e39af07
Zhan Hung Fu <zfuu0016@student.monash.edu>
2024-05-29 23:27:52 +1000

    Completed: - We have implemented a so-called "PowerChitCard" which is essentially a SkipChitCard where it inherits from the abstract Chit Card class. Instead, for the PowerChitCard, its 'symbol_count' now refers to its skip count (i.e. the number of player's turn to skip).
    - Implemented the relevant publishers and listeners for PowerChitCard to notify all subscribed objects that a player has flipped a power chit card.
    
    - Now the 'GameWorld', implements the interface 'PowerChitCardPublisher', where it executes the process of skipping turns by getting the skip count.
    
    TO-DO:
    - Optional: might want to shuffle the chit cards position when a skip chit card is flipped otherwise it would be too crazy
    
    - Adjust the chit card's sizes since we had to modify the size to fit more than 16 chit cards (the chit card area couldn't accomodate for more)
    
    Author: Ian & Desmond

---
+++ Sprint3_Game/game_events/PowerChitCardListener.py
@@ -0,0 +1,20 @@
+from typing import Protocol
+from abc import abstractmethod
+from game_objects.characters.PlayableCharacter import PlayableCharacter
+
+class PowerChitCardListener(Protocol):
+    """
+    Conforms listen for power chit card events that occur in the game.
+
+    Author: Ian & Desmond
+    """
+
+    @abstractmethod
+    def on_action_performed(self,  symbol_count:int):
+        """
+        When an action is performed (i.e. flipped a power chit card), do something.
+
+        Args:
+            character: The character who performed the action
+        """
+        ...

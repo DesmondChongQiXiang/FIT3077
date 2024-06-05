b652e8b80d85397a5586631db403ede8914c55c3
Rohan <rsiv0010@student.monash.edu>
2024-06-06 01:08:39 +1000

    Added the universal enum

--- Sprint3_Game/game_objects/animals/Animal.py
+++ Sprint3_Game/game_objects/animals/Animal.py
@@ -4,10 +4,11 @@ from enum import Enum
 class Animal(Enum):
     """Enum representing the animals that appear on tiles and chit cards.
 
-    Author: Shen
+    Author: Shen,Rohan
     """
 
     BABY_DRAGON = "baby_dragon"
     SALAMANDER = "salamander"
     SPIDER = "spider"
     BAT = "bat"
+    UNIVERSAL = "universal"
